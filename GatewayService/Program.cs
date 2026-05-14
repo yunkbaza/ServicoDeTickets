3d55b3b886c0487bdb85552591f6981f403f798b
yunkbaza <allanbamirati@live.com>
2026-05-14 18:12:40 -0300

    feat: setup CI/CD pipeline com GitHub Actions e Dockerfile

--- GatewayService/Program.cs
+++ GatewayService/Program.cs
@@ -1,14 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Adiciona o serviço do YARP e manda ele ler as regras do appsettings.json
+// 1. CONFIGURAÇÃO DE CORS (Essencial para o Frontend Angular conseguir acessar)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.AllowAnyOrigin()  // Permite qualquer URL (como localhost:4200)
+              .AllowAnyMethod()  // Permite GET, POST, PUT, DELETE
+              .AllowAnyHeader(); // Permite qualquer cabeçalho
+    });
+});
+
+// 2. Adiciona o serviço do YARP lendo do appsettings.json
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
+// 3. ATIVAR O CORS (Tem que vir ANTES do MapReverseProxy)
+app.UseCors("AllowFrontend");
+
 app.MapGet("/", () => "🌐 API Gateway (YARP) está online. Roteando o tráfego da Dropship Hub!");
 
-// 2. Liga a turbina do Proxy
+// 4. Liga a turbina do Proxy
 app.MapReverseProxy();
 
 app.Run();
